a381bb7e38a2902dd08ae5ac41aa31e5913d969a
rocco <roccobuzzomarcelo@gmail.com>
2025-05-08 15:59:32 -0300

    favicon added

--- src/App/views/parts/head.php
+++ src/App/views/parts/head.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/styles.css">
     <link rel="stylesheet" href="../styles/print.css" media="print">
+    <link rel="icon" type="/../images/favicon.ico" href="/images/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
